4687ecb12589369ade09db8827acb724df079e2c
Manoj P R <manojpramesh@gmail.com>
2018-04-26 15:33:27 +0530

    Chapter 1 - Commit

---
+++ Chapter 1/1.4.Working_with_the_JavaScript_console.js
@@ -0,0 +1,21 @@
+// Connec to an exising node
+// $ geth attach http://localhost:8545
+
+// Global web3 object
+web3
+
+// Admin APIs
+admin
+admin.nodeInfo
+admin.peers
+
+// Eth APIs
+eth
+eth.blockNumber
+eth.getBlock(10)
+
+// Other APIs
+personal
+miner
+txpool
+web3.toWei(1, "ether")
