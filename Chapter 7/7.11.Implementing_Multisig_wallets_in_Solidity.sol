cd543015e15494daf17c385c96109998ed888902
Manoj <manojpramesh@gmail.com>
2018-08-07 15:31:23 +0530

    Update 7.11.Implementing_Multisig_wallets_in_Solidity.sol

--- Chapter 7/7.11.Implementing_Multisig_wallets_in_Solidity.sol
+++ Chapter 7/7.11.Implementing_Multisig_wallets_in_Solidity.sol
@@ -1,3 +1,100 @@
+pragma solidity^0.4.24;
+
+contract MultiSig {
+    // Mapping to save the ownership status
+    mapping (address => bool) public isOwner;
+    // Owners array
+    address[] public owners;
+
+    // Returns list of owners.
+    function getOwners() public view returns (address[]) {
+        return owners;
+    }
+
+    // Modifier to restrict access
+    modifier onlyOwner(address owner) {
+        require(isOwner[owner]);
+        _;
+    }
+}
+
+
+uint public required;
+
+constructor(address[] _owners, uint _required) public {
+    for (uint i = 0; i < _owners.length; i++) {
+        require(!(isOwner[_owners[i]] || _owners[i] == 0));
+        isOwner[_owners[i]] = true;
+    }
+    owners = _owners;
+    required = _required;
+}
+
+struct Transaction {
+    address destination;
+    uint value;
+    bytes data;
+    bool executed;
+}
+
+mapping (uint => Transaction) public transactions;
+// Stores the confirmations from each address
+mapping (uint => mapping (address => bool)) public confirmations;
+// Total transaction count for calculating id
+uint public transactionCount;
+
+
+function addTransaction(
+    address destination, 
+    uint value, 
+    bytes data) 
+    internal returns (uint transactionId) 
+{
+    transactionId = transactionCount;
+    transactions[transactionId] = Transaction({
+        destination: destination,
+        value: value,
+        data: data,
+        executed: false
+    });
+    transactionCount += 1;
+    emit Submission(transactionId);
+}
+
+function isConfirmed(uint transactionId) 
+    public constant returns (bool) 
+{
+    uint count = 0;
+    for (uint i=0; i<owners.length; i++) {
+        if (confirmations[transactionId][owners[i]])
+            count += 1;
+        if (count == required)
+            return true;
+    }
+}
+
+
+function executeTransaction(uint transactionId) public {
+    if(isConfirmed(transactionId)) {
+        Transaction storage trnx = transactions[transactionId];
+        if (trnx.destination.call.value(trnx.value)(trnx.data)) {
+            trnx.executed = true;
+            emit Execution(transactionId);
+        }
+    }
+}
+
+
+function confirmTransaction(uint transactionId) 
+    public onlyOwner(msg.sender) 
+{
+    require(!confirmations[transactionId][msg.sender]);
+    confirmations[transactionId][msg.sender] = true;
+    emit Confirmation(msg.sender, transactionId);
+    executeTransaction(transactionId);
+}
+
+
 pragma solidity ^0.4.24;
 
 contract MultiSig {
