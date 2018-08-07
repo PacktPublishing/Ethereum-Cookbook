1c12f5d1f8be71814c5b8241e8643d180fae8a4e
Manoj <manojpramesh@gmail.com>
2018-08-07 17:01:46 +0530

    Update 9.01.Serverless_architecture_for_DApps.js

--- Chapter 9/9.01.Serverless_architecture_for_DApps.js
+++ Chapter 9/9.01.Serverless_architecture_for_DApps.js
@@ -9,6 +9,14 @@
 var TestContract = new web3.eth.contract("<abi>", "<address>");
 TestContract.methods.getData().call().then(console.log);
 
+if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    web3 = new Web3(web3.currentProvider);
+} else {
+    // if no provider is available
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
 TestContract.events.TestEvent({
     filter: {  },
     fromBlock: 0
