baeaefdae7874397357e9e949d7d64e792299506
Manoj <manojpramesh@gmail.com>
2018-08-07 14:58:55 +0530

    Update 6.03.Creating_a_basic_game_on_Ethereum.sol

--- Chapter 6/6.03.Creating_a_basic_game_on_Ethereum.sol
+++ Chapter 6/6.03.Creating_a_basic_game_on_Ethereum.sol
@@ -1,6 +1,62 @@
 pragma solidity ^0.4.23;
 
 contract HeroBattle {
+    //...
+}
+
+struct Hero {
+    string name;
+    uint dna;
+    uint32 level;
+    uint16 winCount;
+    uint16 lossCount;
+}
+    
+Hero[] public heroes;
+
+mapping (uint => address) public heroToOwner;
+ 
+modifier ownerOf(uint _heroId) {
+    require(msg.sender == heroToOwner[_heroId]);
+    _;
+}
+
+function _createHero(string _name, uint _dna) external {
+    //...
+}
+
+uint id = heroes.push(Hero(_name, _dna, 0, 0, 0)) - 1;
+heroToOwner[id] = msg.sender;
+
+// Declaration
+event NewHero(uint heroId, string name, uint dna);
+// Logging
+emit NewHero(id, _name, _dna);
+
+function attack(uint _heroId, uint _targetId) 
+    external 
+    ownerOf(_heroId) {
+    //...
+}
+
+nonce++;
+uint rand = uint(keccak256(now, msg.sender, nonce)) % 100;
+
+if (rand <= 60) { // 60%
+    myHero.winCount++;
+    myHero.level++;
+    enemyHero.lossCount++;
+} else {
+    myHero.lossCount++;
+    enemyHero.winCount++;
+}
+
+emit attackWon(_heroId, _targetId, winnerId)
+
+
+pragma solidity ^0.4.23;
+
+contract HeroBattle {
 
     struct Hero {
         string name;
@@ -11,10 +67,11 @@ contract HeroBattle {
     }
 
     Hero[] public heroes;
-    uint nonce = 0;
-    uint attackerProbability = 75;
 
     mapping (uint => address) public heroToOwner;
+
+    uint nonce = 0;
+    uint attackerProbability = 75;
 
     event NewHero(uint heroId, string name, uint dna);
     event attackWon(uint attackerId, uint targetId, uint winnerId);
@@ -25,10 +82,10 @@ contract HeroBattle {
     }
 
     /**
-    * @dev Create a new hero
-    * @param _name Namme of the hero
-    * @param _dna DNA of the hero
-    */
+     * @dev Create a new hero
+     * @param _name Namme of the hero
+     * @param _dna DNA of the hero
+     */
     function _createHero(string _name, uint _dna) external {
         uint id = heroes.push(Hero(_name, _dna, 0, 0, 0)) - 1;
         heroToOwner[id] = msg.sender;
@@ -36,28 +93,31 @@ contract HeroBattle {
     }
 
     /**
-    * @dev Adds single address to whitelist.
-    * @param _heroId Attacker hero id
-    * @param _targetId Target hero id
-    */
-    function attack(uint _heroId, uint _targetId) external ownerOf(_heroId) {
+     * @dev Adds single address to whitelist.
+     * @param _heroId Attacker hero id
+     * @param _targetId Target hero id
+     */
+    function attack(uint _heroId, uint _targetId) 
+        external ownerOf(_heroId) {
         Hero storage myHero = heroes[_heroId];
         Hero storage enemyHero = heroes[_targetId];
 
         uint winnerId;
+
         nonce++;
         uint rand = uint(keccak256(now, msg.sender, nonce)) % 100;
-        
+
         if (rand <= attackerProbability) {
-            myHero.winCount++;
-            myHero.level++;
-            enemyHero.lossCount++;
-            winnerId = _heroId;
+          myHero.winCount++;
+          myHero.level++;
+          enemyHero.lossCount++;
+          winnerId = _heroId;
         } else {
-            myHero.lossCount++;
-            enemyHero.winCount++;
-            winnerId = _targetId;
+          myHero.lossCount++;
+          enemyHero.winCount++;
+          winnerId = _targetId;
         }
+
         emit attackWon(_heroId, _targetId, winnerId);
     }
 }
